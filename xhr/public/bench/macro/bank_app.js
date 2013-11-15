ee045a9138bb251a5761ae52a0a85485a247f60d
Deian Stefan <deian@cs.stanford.edu>
2013-11-15 10:45:02 -0800

    phone2links bench

---
+++ xhr/public/bench/macro/bank_app.js
@@ -0,0 +1,26 @@
+
+var count = 0;
+
+function main() {
+//  window.console.log("bank_app.js");
+
+  try {
+  var reqA = new XMLHttpRequest();
+
+    reqA.onload = function() { 
+//      window.console.log("w00t onload XHR");
+        var d = document.createElement("div");
+        d.appendChild(document.createTextNode("app: done!"));
+        document.getElementById("log").appendChild(d);
+        done({msg : "xhr complete"});
+    };
+    reqA.open("get", "http://bank.lvh.me:3000/this_is_ok", true);
+    reqA.responseType = "json";
+    reqA.send();
+  } catch(e) { 
+//    window.console.log("bank failed to XHR"); 
+    done({msg : "xhr failed"});
+  }
+}
+
+main();
---
+++ xhr/public/bench/macro/phone.js
@@ -0,0 +1,4 @@
+//console.log("phone.js");
+$(document).ready(function () {
+      $('#log').html( $('#log').html().replace(/(\d?\d?\d-\d\d\d-\d\d\d\d)/g,'<a href="callto://#">$1</a>') );
+});
